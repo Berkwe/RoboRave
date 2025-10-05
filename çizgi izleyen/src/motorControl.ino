2583f54860f34a5eb2f26fc333e434eb6052bb45
Berkwe <durgunberke64@gmail.com>
2025-10-05 15:53:53 +0300

    bişiler eklendi

--- çizgi izleyen/src/motorControl.ino
+++ çizgi izleyen/src/motorControl.ino
@@ -9,8 +9,8 @@
 
 #define PIN_motorRightSpeed 5 // sağ motor PWM
 #define PIN_motorLeftSpeed 6 // sol motor PWM
-#define PIN_motorRight 8 // Sağ motor yön
-#define PIN_motorLeft 7 // Sol motor yön
+#define PIN_motorRight 7 // Sağ motor yön
+#define PIN_motorLeft 8 // Sol motor yön
 #define PIN_motorEN 3 // Motor sürücüsü enable pini
 
 #define PIN_button 2 // d2 Button pini
@@ -19,12 +19,17 @@
 
 // renkler
 CRGB leds[1];
-CRGB red = CRGB(255,0,0);
-CRGB green = CRGB(0,255,0);
-CRGB blue = CRGB(0,0,255);
-CRGB cyan = CRGB(0,255,255);
-CRGB white = CRGB(255,255,255);
-CRGB black = CRGB(0,0,0);
+// kalibrasyon
+CRGB red = CRGB(255,0,0); // kalibrasyon uyarısı rengi
+CRGB blue = CRGB(0,0,255); // kalibrasyon çizgi algılama rengi
+CRGB white = CRGB(255,255,255); // kalbirasyon arka plan algılama rengi
+CRGB green = CRGB(0,255,0); // tamamlanma rengi
+// uyarılar
+CRGB cyan = CRGB(0,255,255); // robot havada rengi
+CRGB yellow = CRGB(255,255,0); // çizgiye yerleştirin rengi
+// diğerleri
+CRGB black = CRGB(0,0,0); // led kapatma rengi
+
 
 
 #define speedMax 200 // en fazla hız
@@ -32,7 +37,7 @@ CRGB black = CRGB(0,0,0);
 
 
 bool isReversedLines = false; // çizginin arka plandan daha açık olduğunu belirten flag
-int lineDetectionValue = 850; // (|arka_plan_değeri-çizgi_değeri|)/2 böylece dinamik doğru bi dğeer atanır
+int lineDetectionValue = 700; // (|arka_plan_değeri-çizgi_değeri|)/2 böylece dinamik doğru bi dğeer atanır
 
 unsigned long previousTime = 0; // delaySenc için
 
@@ -42,10 +47,6 @@ int buttonMode = 1; // mod değeri
 
 bool isCalibrated = false; // kalibre edildi mi
 bool isCarOnGround = false; // robot yerde mi
-
-bool sensorStateLeft;
-bool sensorStateMiddle;
-bool sensorStateRight;
 
 
 // PID değişkenleri
@@ -62,15 +63,26 @@ unsigned long g_blindStartTime = 0;
 bool g_blindActive = false;
 float g_lastError = 0;
 
+bool debug = false;
 
-bool delaySenc(int ms) {
-    static unsigned long prevTime = 0;
+
+// Otomatik timer yönetimi için
+enum TimerID {
+    TIMER_LED,
+    TIMER_BUTTON,
+    TIMER_LINE_DETECT,
+    TIMER_COUNT  // Toplam timer sayısı
+};
+
+unsigned long g_timers[TIMER_COUNT] = {0};
+
+bool delaySenc(TimerID id, int ms) {
     unsigned long currentTime = millis();
-    if (currentTime - prevTime >= ms) {
-        prevTime = currentTime;
+    if (currentTime - g_timers[id] >= ms) {
+        g_timers[id] = currentTime;
         return true;
     }
-    else return false;
+    return false;
 }
 
 
@@ -82,13 +94,13 @@ void burnLed(CRGB color, int brightness = 255) {
 }
 
 
-void blinkLed(bool var, CRGB color, int interval) {
+void blinkLed(bool var, CRGB color, int interval = 500) {
     if (var) {
         burnLed(black);
         return;
     }
 
-    if (delaySenc(interval)) {
+    if (delaySenc(TIMER_LED, interval)) {
         bool ledState = constrain(FastLED.getBrightness(), 0, 1);
         if (!ledState) {
             burnLed(color);
@@ -104,13 +116,15 @@ void controlCarGround() {
     int sensorR = analogRead(PIN_irRight);
     if (sensorR > minGroundDistance && sensorM > minGroundDistance && sensorL > minGroundDistance) {
         isCarOnGround = false;
-        if (buttonMode == 1 && isCalibrated) {
+        if (buttonMode == 1 && isCalibrated) { // havada
+            digitalWrite(PIN_motorEN, 0); // motru kapat
             burnLed(cyan);
         }
     }
-    else {
+    else { // yerde
         isCarOnGround = true;
         if (buttonMode == 1 && isCalibrated) {
+            digitalWrite(PIN_motorEN, 1);
             burnLed(black);
         }
     }
@@ -149,16 +163,75 @@ enum mods {
 };
 
 
+void goTo(directions direction, uint8_t speed = 0, uint8_t speedRight = 0, uint8_t speedLeft = 0) { // motoru hareket ettirir
+    if (!isCalibrated || !isCarOnGround) return;
+    
+    speed = constrain(speed, 0, speedMax);
+    switch (direction)
+    {
+    case forward:
+        digitalWrite(PIN_motorRight, 1);
+        analogWrite(PIN_motorRightSpeed, (speed == 0 ? speedRight : speed));
+        digitalWrite(PIN_motorLeft, 1);
+        analogWrite(PIN_motorLeftSpeed, speed == 0 ? speedLeft : speed);
+        break;
+    case backward:
+        digitalWrite(PIN_motorRight, 0);
+        analogWrite(PIN_motorRightSpeed, (speed == 0 ? speedRight : speed));
+        digitalWrite(PIN_motorLeft, 0);
+        analogWrite(PIN_motorLeftSpeed, speed == 0 ? speedLeft : speed);
+        break;
+    case left:
+        digitalWrite(PIN_motorRight, 1);
+        analogWrite(PIN_motorRightSpeed, (speed == 0 ? speedRight : speed));
+        digitalWrite(PIN_motorLeft, 0);
+        analogWrite(PIN_motorLeftSpeed, speed == 0 ? speedLeft : speed);
+        break;
+    case right:
+        digitalWrite(PIN_motorRight, 0);
+        analogWrite(PIN_motorRightSpeed, (speed == 0 ? speedRight : speed));
+        digitalWrite(PIN_motorLeft, 1);
+        analogWrite(PIN_motorLeftSpeed, speed == 0 ? speedLeft : speed);
+        break;
+    case leftForward:
+        digitalWrite(PIN_motorRight, 1);
+        analogWrite(PIN_motorRightSpeed, (speed == 0 ? speedRight : speed));
+        analogWrite(PIN_motorLeftSpeed, 0);
+        break;
+    case leftBackward:
+        digitalWrite(PIN_motorRight, 0);
+        analogWrite(PIN_motorRightSpeed, (speed == 0 ? speedRight : speed));
+        analogWrite(PIN_motorLeftSpeed, 0);
+        break;
+    case rightForward:
+        analogWrite(PIN_motorRightSpeed, 0);
+        digitalWrite(PIN_motorLeft, 1);
+        analogWrite(PIN_motorLeftSpeed, speed == 0 ? speedLeft : speed);
+        break;
+    case rightBackward:
+        analogWrite(PIN_motorRightSpeed, 0);
+        digitalWrite(PIN_motorLeft, 0);
+        analogWrite(PIN_motorLeftSpeed, speed == 0 ? speedLeft : speed);
+        break;
+    case stop_it:
+        analogWrite(PIN_motorRightSpeed, 0);
+        analogWrite(PIN_motorLeftSpeed, 0);
+        break;
+    default:
+        break;
+    }
 
+}
 
 
 void calibrateSensors() { // İr sensörleri arka planla kalibre eder ivmeölçeri falan da aynı şekilde
+    digitalWrite(PIN_motorEN, 0);
     burnLed(white);
     delay(2000);
-    Serial.println("Kalibrasyon başladı");
-    int sensorL = analogRead(PIN_irLeft);
-    int sensorM = analogRead(PIN_irMiddle);
-    int sensorR = analogRead(PIN_irRight);
+    if (debug) Serial.println("Kalibrasyon başladı");
+    unsigned long sensorL = analogRead(PIN_irLeft);
+    unsigned long sensorM = analogRead(PIN_irMiddle);
+    unsigned long sensorR = analogRead(PIN_irRight);
     int backgroundDetectionValue;
     int localLineDetectionValue;
     // arka plan kalibrasyonu
@@ -182,22 +255,58 @@ void calibrateSensors() { // İr sensörleri arka planla kalibre eder ivmeölçe
     burnLed(black);
     delay(100);
 
-    // çizgi kalibrasyonu
-    sensorL = analogRead(PIN_irLeft);
-    sensorM = analogRead(PIN_irMiddle);
-    sensorR = analogRead(PIN_irRight);
+    // çizgi kalibrasyonu 50
+    sensorL = 0;
+    sensorM = 0;
+    sensorR = 0;
+    int sensorLV = 1;
+    int sensorMV = 1;
+    int sensorRV = 1;
     burnLed(blue);
     delay(2000);
-    burnLed(black);
-    for (int i = 0; i < 3; i++)
+    burnLed(green);
+    for (int i = 0; i < 30; i++)
         {
-            sensorL += analogRead(PIN_irLeft);
-            sensorM += analogRead(PIN_irMiddle);
-            sensorR += analogRead(PIN_irRight);
+            int L_sensorL = analogRead(PIN_irLeft);
+            int L_sensorM = analogRead(PIN_irMiddle);
+            int L_sensorR = analogRead(PIN_irRight);
+
+            // önce arka planın koyu mu açık mı olduğu tespit ediliyor sonrasında okunan değerin arka plana göre çizgi olup olmadığını algılayıp kaydediyor
+
+            if (backgroundDetectionValue <= 512) {
+                if (L_sensorL > backgroundDetectionValue) {
+                    sensorL += L_sensorL;
+                    sensorLV++;
+                }
+                if (L_sensorM > backgroundDetectionValue) {
+                    sensorM += L_sensorM;
+                    sensorMV++;
+                }
+                if (L_sensorR > backgroundDetectionValue) {
+                    sensorR += L_sensorR;
+                    sensorRV++;
+                }
+            }
+            else {
+                if (L_sensorL < backgroundDetectionValue) {
+                    sensorL += L_sensorL;
+                    sensorLV++;
+                }
+                if (L_sensorM < backgroundDetectionValue) {
+                    sensorM += L_sensorM;
+                    sensorMV++;
+                }
+                if (L_sensorR < backgroundDetectionValue) {
+                    sensorR += L_sensorR;
+                    sensorRV++;
+                }
+            }
+            
+        delay(30);
         }
-    sensorL /= 3.0;
-    sensorM /= 3.0;
-    sensorR /= 3.0;
+    sensorL /= sensorLV;
+    sensorM /= sensorMV;
+    sensorR /= sensorRV;
     localLineDetectionValue = (sensorL+sensorM+sensorR)/3; // üçünün ortalaması
     burnLed(green);
     delay(100);
@@ -208,76 +317,18 @@ void calibrateSensors() { // İr sensörleri arka planla kalibre eder ivmeölçe
     burnLed(black);
     delay(100);
     isCalibrated = true;
-    lineDetectionValue = abs((backgroundDetectionValue-localLineDetectionValue))/2;
+    lineDetectionValue = abs((backgroundDetectionValue+localLineDetectionValue))/2;
     if (lineDetectionValue>backgroundDetectionValue) isReversedLines = true; // eğer çizgi açık bir renk ise
     digitalWrite(PIN_motorEN, 1);
-    Serial.println("\narka plan : ");
-    Serial.print(backgroundDetectionValue);
-    Serial.println("\nçizgi : ");
-    Serial.print(localLineDetectionValue);
-}
-
-
-void goTo(directions direction, uint8_t speed = 0) { // motoru hareket ettirir
-    if (!isCalibrated) return;
-    
-
-    speed = constrain(speed, 0, speedMax);
-    switch (direction)
-    {
-    case forward:
-        digitalWrite(PIN_motorRight, 1);
-        analogWrite(PIN_motorRightSpeed, speed);
-        digitalWrite(PIN_motorLeft, 1);
-        analogWrite(PIN_motorLeftSpeed, speed);
-        break;
-    case backward:
-        digitalWrite(PIN_motorRight, 0);
-        analogWrite(PIN_motorRightSpeed, speed);
-        digitalWrite(PIN_motorLeft, 0);
-        analogWrite(PIN_motorLeftSpeed, speed);
-        break;
-    case left:
-        digitalWrite(PIN_motorRight, 1);
-        analogWrite(PIN_motorRightSpeed, speed);
-        digitalWrite(PIN_motorLeft, 0);
-        analogWrite(PIN_motorLeftSpeed, speed);
-        break;
-    case right:
-        digitalWrite(PIN_motorRight, 0);
-        analogWrite(PIN_motorRightSpeed, speed);
-        digitalWrite(PIN_motorLeft, 1);
-        analogWrite(PIN_motorLeftSpeed, speed);
-        break;
-    case leftForward:
-        digitalWrite(PIN_motorRight, 1);
-        analogWrite(PIN_motorRightSpeed, speed);
-        analogWrite(PIN_motorLeftSpeed, 0);
-        break;
-    case leftBackward:
-        digitalWrite(PIN_motorRight, 0);
-        analogWrite(PIN_motorRightSpeed, speed);
-        analogWrite(PIN_motorLeftSpeed, 0);
-        break;
-    case rightForward:
-        analogWrite(PIN_motorRightSpeed, 0);
-        digitalWrite(PIN_motorLeft, 1);
-        analogWrite(PIN_motorLeftSpeed, speed);
-        break;
-    case rightBackward:
-        analogWrite(PIN_motorRightSpeed, 0);
-        digitalWrite(PIN_motorLeft, 0);
-        analogWrite(PIN_motorLeftSpeed, speed);
-        break;
-    case stop_it:
-        analogWrite(PIN_motorRightSpeed, 0);
-        analogWrite(PIN_motorLeftSpeed, 0);
-        break;
-    default:
-        break;
+    if (debug) {
+        Serial.println("\narka plan : ");
+        Serial.print(backgroundDetectionValue);
+        Serial.println("\nçizgi : ");
+        Serial.print(localLineDetectionValue);
     }
 
 }
+
 
 
 void setPIDParameters(float kp, float ki, float kd, int baseSpeed) {
@@ -319,32 +370,45 @@ void followLine() { // çizgi takibi
         g_blindActive = false;
         return;
     }
-
+    
     int sensorL = analogRead(PIN_irLeft);
     int sensorM = analogRead(PIN_irMiddle);
     int sensorR = analogRead(PIN_irRight);
 
-    bool left = isReversedLines ? (sensorL > lineDetectionValue) : (sensorL < lineDetectionValue);
-    bool middle = isReversedLines ? (sensorM > lineDetectionValue) : (sensorM < lineDetectionValue);
-    bool right = isReversedLines ? (sensorR > lineDetectionValue) : (sensorR < lineDetectionValue);
-
+    
+    int left = !isReversedLines ? (sensorL < lineDetectionValue) : (sensorL >= lineDetectionValue);
+    int middle = !isReversedLines ? (sensorM < lineDetectionValue) : (sensorM >= lineDetectionValue);
+    int right = !isReversedLines ? (sensorR < lineDetectionValue) : (sensorR >= lineDetectionValue);
+    
+    if (debug) {
+        Serial.print("SensorL: "); Serial.print(sensorL);
+        Serial.print("  Left: "); Serial.print(left);
+        Serial.print(" | SensorM: "); Serial.print(sensorM);
+        Serial.print("  Middle: "); Serial.print(middle);
+        Serial.print(" | SensorR: "); Serial.print(sensorR);
+        Serial.print("  Right: "); Serial.println(right);
+    }
+    
     if (!left && !middle && !right) {
+        g_integral = 0;
+        g_previousError = 0;
+        g_lastPIDTime = 0;
+        
         if (!g_blindActive) {
             g_blindActive = true;
             g_blindStartTime = millis();
-            g_integral = 0;
-            g_previousError = 0;
-            g_lastPIDTime = 0;
         }
         blindDetection();
         return;
     }
-
+    
     g_blindActive = false;
 
+
+    // -1 sol 1 sağ
     float totalWeight = left + middle + right;
-    float error = (-1.0 * left + 0.0 * middle + 1.0 * right) / totalWeight;
-    g_lastError = error;
+    float error = (-1.0 * left + 0 * middle + 1.0 * right) / totalWeight;
+
 
     unsigned long currentTime = millis();
     if (g_lastPIDTime == 0) {
@@ -354,81 +418,72 @@ void followLine() { // çizgi takibi
         return;
     }
 
+    // P terimi orantısal hata
     float dt = (currentTime - g_lastPIDTime) / 1000.0;
-    if (dt <= 0 || dt > 1.0) dt = 0.02;
+    if (dt <= 0 || dt > 1.0) dt = 0.02; // dt önceki PID dögüsünden bi sonraki döngüye kadar ne kadar zaman geçtiğini hesaplıyor
     g_lastPIDTime = currentTime;
 
-    float P = g_Kp * error;
-
-    if (abs(error) < 0.01) g_integral = 0;
+    float P = g_Kp * error; // o anki hatayı orantılı düzeltiyor. mesela -0.5 (çizginin hafif solunda) gibi bir hata var kp = 45.0 P = -22.5 oluyor robot hesaplanan hızda düzelene kadar sola gider
+    
+    
+    // i terimi INTEGRAL
+    if (abs(error) < 0.01) g_integral = 0; // o ana kadarki yapılan tüm hatalara göre düzeltiyor
     else {
         g_integral += error * dt;
         g_integral = constrain(g_integral, -50.0, 50.0);
     }
     float I = g_Ki * g_integral;
+    // mesela Ki = 0.08 diyelim error 3 döngüde sırayla 0.5 1 -0.5 gibi değerler alsın dt ye de 0.05 diyelim (50 ms)
+    // g_integral = (0.05 * 0.5) + (0.05 * 1) + (0.05 * -0.5) = 0.0725 bu sayıyı -50 ve 50 yi geçmiyecek şekilde sınırlandııryoruz
+    // I = 0.08 * 0.0725 = 0,0058
 
-    float derivative = (error - g_previousError) / dt;
+
+    // D terimi TÜREV
+    float derivative = (error - g_previousError) / dt; // hatanın ne kadar hızlı düzeldiğini hesaplayıp ona göre düzeltme yapar
     float D = g_Kd * derivative;
     g_previousError = error;
+    // mesela dt = 0.05 (50 ms) Kd = 12.0, error = 1 (tam sağda), prevError = 0 (tam ortada) olsun 
+    // derivative = (1 - 0) / 0.05 = 20 olur
+    // D = 12 * 20 = 240 olur
+    // Sonraki döngüde dt = 0.05, error = 0.5, prevError = 1 diyelim
+    // derivative = (0.5 - 1) / 0.05 = -10 
+    // D = 12 * -10 = -120 olur
+    
+    // PID
+    int correction = P+I+D;
 
-    int correction = P + I + D;
-    int leftSpeed = g_baseSpeed - correction;
-    int rightSpeed = g_baseSpeed + correction;
+    // motor hızları PID ye göre hesaolanıyor
+    int  leftSpeed = g_baseSpeed - correction;
+    int  rightSpeed = g_baseSpeed + correction;
 
     leftSpeed = constrain(leftSpeed, 0, speedMax);
     rightSpeed = constrain(rightSpeed, 0, speedMax);
 
-    digitalWrite(PIN_motorRight, 1);
-    analogWrite(PIN_motorRightSpeed, rightSpeed);
-    digitalWrite(PIN_motorLeft, 1);
-    analogWrite(PIN_motorLeftSpeed, leftSpeed);
-}
+    goTo(forward, 0, leftSpeed, rightSpeed);
 
+}   
 
-void controlLine() { // çizgi kontrolü
-    if (!isCalibrated) return;
-    if (!isCarOnGround) return;
-
-    int sensorStateLeft = analogRead(PIN_irLeft);
-    int sensorStateMiddle = analogRead(PIN_irMiddle);
-    int sensorStateRight = analogRead(PIN_irRight);
-
-    if (!isReversedLines) {
-        if (sensorStateLeft < lineDetectionValue) sensorStateLeft = 1;
-        else sensorStateLeft = 0;
-        if (sensorStateMiddle < lineDetectionValue) sensorStateMiddle = 1;
-        else sensorStateMiddle = 0;
-        if (sensorStateRight < lineDetectionValue) sensorStateRight = 1;
-        else sensorStateRight = 0;
-    }
-    else {
-        if (sensorStateLeft > lineDetectionValue) sensorStateLeft = 1;
-        else sensorStateLeft = 0;
-        if (sensorStateMiddle > lineDetectionValue) sensorStateMiddle = 1;
-        else sensorStateMiddle = 0;
-        if (sensorStateRight > lineDetectionValue) sensorStateRight = 1;
-        else sensorStateRight = 0;
-    }
-}
 
 void setup() {
     Serial.begin(9600);
     buttonMode = 1;
     PinInit();
-
+    if (debug) {
+        burnLed(blue);
+        delay(1500);
+        burnLed(black);
+    }
 }
 
 
-
 void loop() {
-
     blinkLed(isCalibrated, red, 500);
     controlCarGround();
 
     // Mod seçimi
-    if (!digitalRead(PIN_button) && delaySenc(70)) { 
+    if (!digitalRead(PIN_button) && delaySenc(TIMER_BUTTON, 70)) { 
         if (buttonMode < 2) buttonMode += 1; 
-        else buttonMode = 1; 
+        else buttonMode = 1;
     }
 
     // Mod kontrolü
@@ -441,5 +496,7 @@ void loop() {
             buttonMode = 1;
             break;
     }
+    
+    
 }
 
